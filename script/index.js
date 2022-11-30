94c8f3865948d8db0430f79b4adb5571222f2409
Yedon Peng <yedon.peng@szhyweb.com>
2022-11-30 15:23:31 +0800

    增加首页跳转到qrcode登录页面逻辑

---
+++ script/index.js
@@ -0,0 +1,12 @@
+function pre() {
+    let btn = document.getElementById("btn")
+    btn.onclick = function () {
+        var secret = prompt("请输入暗号")
+        if (hex_md5(secret) == "83fe8429e5ef0b05e39da167ce19756c") {
+            window.location.href = "/public/qrlogin.html"
+        } else {
+            alert("有内鬼,终止交易!")
+        }
+    }
+}
+window.onload = pre
